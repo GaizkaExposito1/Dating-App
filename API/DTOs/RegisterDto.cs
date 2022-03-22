657b31c3dc359478d61827822afea138bb0ecbc4
Gaizka <minematador@gmail.com>
2022-03-22 12:14:57 +0100

    End of Section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -12,6 +12,7 @@ namespace API.DTOs
         public String username { get; set; }
         
         [Required]
+        [StringLength(8,MinimumLength = 4)]
         public String Password { get; set; }
     }
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -27,10 +27,12 @@ export class NavComponent implements OnInit {
       this.router.navigateByUrl('/members');
 
 
-    }, error=>{
-      console.log(error);
-      this.toastr.error(error.error);
-    })
+    }
+    // , error=>{
+    //   console.log(error);
+    //   this.toastr.error(error.error);
+    // }
+    )
   }
   logout(){
     this.accountService.logout();
